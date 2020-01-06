7086d81d28155735ee0f121312590ccad3963482
Chaitanya <chaitanyakulkarni0509@gmail.com>
2020-01-06 13:24:10 -0800

    full code

---
+++ src/main/java/googleAPIs/resources1.java
@@ -0,0 +1,16 @@
+package googleAPIs;
+
+public class resources1 {
+
+	public static String placePostData()
+	{
+		
+		String res="/maps/api/place/add/json";
+		return res;
+	}
+	
+	
+	
+
+
+}
---
+++ src/main/java/jiraAPI/resources1.java
@@ -0,0 +1,16 @@
+package jiraAPI;
+
+public class resources1 {
+
+	public static String placePostData()
+	{
+		
+		String res="/rest/api/2/issue/10207/comment/";
+		return res;
+	}
+	
+	
+	
+
+
+}
